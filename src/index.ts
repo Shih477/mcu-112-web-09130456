b6e994be816a06da8ed975efd6be6a94da6f80e9
Shih477 <q10301761968@gmail.com>
2023-11-03 15:58:51 +0800

    feat(ts): 利用布林變數記錄待辦事項是否完成

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 let todoId: number;
 todoId = 1;
 console.log("Todo ID: ", todoId);
+
 const todo = "期中考作業 ";
 console.log("Todo Name: ", todo);
+
+const hasFinished = false;
+console.log("Has Finished:", hasFinished);
