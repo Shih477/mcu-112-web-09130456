bec2dd84b5c398f27ab7634464ebdf187b917b45
Shih477 <q10301761968@gmail.com>
2023-12-01 16:13:20 +0800

    feat(todo-list): 無待辧事項顥示實作

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,18 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Todo } from '../model/todo';
 import { TodoComponent } from '../todo/todo.component';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 @Component({
   selector: 'app-todo-list',
   standalone: true,
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css',
-  imports: [TodoComponent, NgFor],
+  imports: [TodoComponent, NgFor, NgIf],
 })
 export class TodoListComponent {
   @Input()
   tasks!: Todo[];
 
   @Output()
-  StateChange = new EventEmitter<{ index: number; state: boolean }>();
+  stateChange = new EventEmitter<{ index: number; state: boolean }>();
 }
