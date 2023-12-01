84e1f4f9f7b61fef42457736c08fc2130a140f66
Shih477 <q10301761968@gmail.com>
2023-12-01 15:24:31 +0800

    feat(todo-list): 實作顯示多筆代辦事項清單

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { HeaderComponent } from './header/header.component';
 import { Component } from '@angular/core';
-import { TodoComponent } from './todo/todo.component';
 import { FooterComponent } from './footer/footer.component';
 import { Todo } from './model/todo';
 import { JsonPipe } from '@angular/common';
@@ -9,16 +8,19 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    HeaderComponent,
-    TodoComponent,
-    FooterComponent,
-    JsonPipe,
-    TodoListComponent,
-  ],
+  imports: [HeaderComponent, FooterComponent, JsonPipe, TodoListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  task = new Todo(1, '待辦事項 A');
+  tasks = [new Todo(1, '待辦事項 A'), new Todo(2, '待辦事項 B')];
+
+  onStateChange(task: { index: number; state: boolean }): void {
+    if (task.state) {
+      this.tasks[task.index].setFinished(new Date());
+    } else {
+      this.tasks[task.index].finishDate = undefined;
+      this.tasks[task.index].hasFinished = false;
+    }
+  }
 }
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,22 +1,18 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Todo } from '../model/todo';
 import { TodoComponent } from '../todo/todo.component';
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-todo-list',
   standalone: true,
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css',
-  imports: [TodoComponent],
+  imports: [TodoComponent, NgFor],
 })
 export class TodoListComponent {
-  task = new Todo(1, '待辦事項 A');
-  onStateChange(state: boolean): void {
-    if (state) {
-      this.task.setFinished(new Date());
-    } else {
-      this.task.finishDate = undefined;
-      this.task.hasFinished = false;
-    }
-  }
+  @Input()
+  tasks!: Todo[];
+
+  @Output()
+  StateChange = new EventEmitter<{ index: number; state: boolean }>();
 }
