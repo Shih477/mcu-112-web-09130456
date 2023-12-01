670493ca71e35102af826ec77697e1da08bdb2be
Shih477 <q10301761968@gmail.com>
2023-12-01 16:42:04 +0800

    feat(task): 待辦事項資料變更從服務取得

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
-import { HeaderComponent } from './header/header.component';
-import { Component } from '@angular/core';
-import { FooterComponent } from './footer/footer.component';
-import { Todo } from './model/todo';
 import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { Todo } from './model/todo';
+import { TaskService } from './services/task.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, JsonPipe, TodoListComponent],
+  imports: [HeaderComponent, TodoListComponent, FooterComponent, JsonPipe],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  taskService = inject(TaskService);
+
   tasks = [new Todo(1, '待辦事項 A'), new Todo(2, '待辦事項 B')];
 
   onStateChange(task: { index: number; state: boolean }): void {
