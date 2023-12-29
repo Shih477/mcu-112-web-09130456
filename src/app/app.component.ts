42f445fb4e32ed0fed750f84ee3c89e540849e8b
Shih477 <q10301761968@gmail.com>
2023-12-29 14:57:43 +0800

    feat(home): 待辦事項頁面元件實作新增

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,73 +1,10 @@
-import { AsyncPipe, NgIf } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { Todo } from './model/todo';
-import { TodoDetailComponent } from './todo-detail/todo-detail.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
-import {
-  BehaviorSubject,
-  Observable,
-  Subject,
-  merge,
-  startWith,
-  switchMap,
-} from 'rxjs';
-import { TaskService } from './services/task.service';
-import { TodoSearchComponent } from './todo-search/todo-search.component';
-import { TodoFormComponent } from './todo-form/todo-form.component';
+import { Component } from '@angular/core';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    NgIf,
-    AsyncPipe,
-    NavBarComponent,
-    HeaderComponent,
-    TodoListComponent,
-    TodoDetailComponent,
-    TodoSearchComponent,
-    TodoFormComponent,
-    FooterComponent,
-  ],
+  imports: [NavBarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  taskService = inject(TaskService);
-
-  tasks$!: Observable<Todo[]>;
-
-  readonly search$ = new BehaviorSubject<string | null>(null);
-
-  readonly refresh$ = new Subject<void>();
-
-  selectedId?: number;
-
-  ngOnInit(): void {
-    this.tasks$ = merge(
-      this.refresh$.pipe(startWith(undefined)),
-      this.search$
-    ).pipe(switchMap(() => this.taskService.getAll(this.search$.value)));
-  }
-
-  onSave(task: Todo): void {
-    this.taskService.add(task).subscribe(() => this.refresh$.next());
-  }
-
-  onSearch(content: string | null): void {
-    this.search$.next(content);
-  }
-
-  onRemove(id: number): void {
-    this.taskService.remove(id).subscribe(() => this.refresh$.next());
-  }
-
-  onStateChange({ task, state }: { task: Todo; state: boolean }): void {
-    this.taskService
-      .updateState(task, state)
-      .subscribe(() => this.refresh$.next());
-  }
-}
+export class AppComponent {}
---
+++ src/app/todo-page/todo-page.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
+import { TodoDetailComponent } from '../todo-detail/todo-detail.component';
+import { TodoFormComponent } from '../todo-form/todo-form.component';
+import { TodoListComponent } from '../todo-list/todo-list.component';
+import { TodoSearchComponent } from '../todo-search/todo-search.component';
+import {
+  Observable,
+  BehaviorSubject,
+  Subject,
+  merge,
+  startWith,
+  switchMap,
+} from 'rxjs';
+import { Todo } from '../model/todo';
+import { TaskService } from '../services/task.service';
+
+@Component({
+  selector: 'app-todo-page',
+  standalone: true,
+  imports: [
+    NgIf,
+    AsyncPipe,
+    HeaderComponent,
+    TodoListComponent,
+    TodoDetailComponent,
+    TodoSearchComponent,
+    TodoFormComponent,
+    FooterComponent,
+  ],
+  templateUrl: './todo-page.component.html',
+  styleUrl: './todo-page.component.css',
+})
+export class TodoPageComponent implements OnInit {
+  taskService = inject(TaskService);
+
+  tasks$!: Observable<Todo[]>;
+
+  readonly search$ = new BehaviorSubject<string | null>(null);
+
+  readonly refresh$ = new Subject<void>();
+
+  selectedId?: number;
+
+  ngOnInit(): void {
+    this.tasks$ = merge(
+      this.refresh$.pipe(startWith(undefined)),
+      this.search$
+    ).pipe(switchMap(() => this.taskService.getAll(this.search$.value)));
+  }
+
+  onSave(task: Todo): void {
+    this.taskService.add(task).subscribe(() => this.refresh$.next());
+  }
+
+  onSearch(content: string | null): void {
+    this.search$.next(content);
+  }
+
+  onRemove(id: number): void {
+    this.taskService.remove(id).subscribe(() => this.refresh$.next());
+  }
+
+  onStateChange({ task, state }: { task: Todo; state: boolean }): void {
+    this.taskService
+      .updateState(task, state)
+      .subscribe(() => this.refresh$.next());
+  }
+}
